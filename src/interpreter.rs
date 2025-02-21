c28d126c7809af33b9e2acb5be7d3b0181c3ab9f
Alexander Flink <hi@alexanderflink.com>
2025-02-21 21:59:01 +0100

    add string, subtraction, multiplication, division

---
+++ src/interpreter.rs
@@ -0,0 +1,137 @@
+use std::{
+    collections::HashMap,
+    fmt::Display,
+    ops::{Add, Div, Mul, Sub},
+};
+
+use crate::parser::*;
+
+impl Expression {
+    fn evaluate(&self, variables: &HashMap<String, VariableValue>) -> VariableValue {
+        match self {
+            Expression::Term(term) => term.evaluate(variables),
+            Expression::Operation { left, op, right } => {
+                let left = left.evaluate(variables);
+                let right = right.evaluate(variables);
+
+                match op {
+                    Operator::Add => left + right,
+                    Operator::Subtract => left - right,
+                    Operator::Multiply => left * right,
+                    Operator::Divide => left / right,
+                }
+            }
+        }
+    }
+}
+
+impl Term {
+    fn evaluate(&self, variables: &HashMap<String, VariableValue>) -> VariableValue {
+        match self {
+            Term::Number(number) => VariableValue::Integer(*number),
+            Term::String(string) => VariableValue::String(string.clone()),
+            Term::Variable(variable) => {
+                let Some(value) = variables.get(variable) else {
+                    panic!("Kunde inte hitta variabeln \"{}\"", variable);
+                };
+
+                match value {
+                    VariableValue::Integer(number) => VariableValue::Integer(*number),
+                    VariableValue::String(string) => VariableValue::String(string.clone()),
+                }
+            }
+        }
+    }
+}
+
+pub struct Interpreter {
+    variables: HashMap<String, VariableValue>,
+}
+
+pub enum VariableValue {
+    Integer(i64),
+    String(String),
+}
+
+impl Add for VariableValue {
+    fn add(self, other: Self) -> Self {
+        match (self, other) {
+            (Self::Integer(a), Self::Integer(b)) => Self::Integer(a + b),
+            (Self::Integer(a), Self::String(b)) => Self::String(format!("{}{}", a, b)),
+            (Self::String(a), Self::Integer(b)) => Self::String(format!("{}{}", a, b)),
+            (Self::String(a), Self::String(b)) => Self::String(format!("{}{}", a, b)),
+        }
+    }
+
+    type Output = Self;
+}
+
+impl Sub for VariableValue {
+    fn sub(self, other: Self) -> Self {
+        match (self, other) {
+            (Self::Integer(a), Self::Integer(b)) => Self::Integer(a - b),
+            _ => panic!("Kan inte subtrahera olika typer av värden"),
+        }
+    }
+
+    type Output = Self;
+}
+
+impl Mul for VariableValue {
+    fn mul(self, other: Self) -> Self {
+        match (self, other) {
+            (Self::Integer(a), Self::Integer(b)) => Self::Integer(a * b),
+            _ => panic!("Kan inte multiplicera olika typer av värden"),
+        }
+    }
+
+    type Output = Self;
+}
+
+impl Div for VariableValue {
+    fn div(self, other: Self) -> Self {
+        match (self, other) {
+            (Self::Integer(a), Self::Integer(b)) => Self::Integer(a / b),
+            _ => panic!("Kan inte dividera olika typer av värden"),
+        }
+    }
+
+    type Output = Self;
+}
+
+impl Display for VariableValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            VariableValue::Integer(number) => write!(f, "{}", number),
+            VariableValue::String(string) => write!(f, "{}", string),
+        }
+    }
+}
+
+impl Interpreter {
+    pub fn new() -> Interpreter {
+        Interpreter {
+            variables: HashMap::new(),
+        }
+    }
+
+    pub fn interpret(&mut self, statements: Vec<Statement>) {
+        statements.iter().for_each(|statement| match statement {
+            Statement::Assignment {
+                variable,
+                expression,
+            } => match variable {
+                Term::Variable(identifier) => {
+                    self.variables
+                        .insert(identifier.clone(), expression.evaluate(&self.variables));
+                }
+                _ => {
+                    panic!("Expected variable identifier");
+                }
+            },
+            Statement::Print { expression } => {
+                println!("{}", expression.evaluate(&self.variables));
+            }
+        });
+    }
+}
---
+++ src/parser.rs
@@ -0,0 +1,155 @@
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, tag_no_case, take_while1},
+    character::complete::{multispace0, space1},
+    combinator::{cut, opt},
+    multi::many1,
+    sequence::delimited,
+    IResult, Parser as NomParser,
+};
+
+#[derive(Debug)]
+pub enum Statement {
+    Assignment {
+        variable: Term,
+        expression: Expression,
+    },
+    Print {
+        expression: Expression,
+    },
+}
+
+#[derive(Debug)]
+pub enum Expression {
+    Term(Term),
+    Operation {
+        left: Box<Expression>,
+        op: Operator,
+        right: Box<Expression>,
+    },
+}
+
+#[derive(Debug)]
+pub enum Term {
+    Number(i64),
+    String(String),
+    Variable(String),
+}
+
+#[derive(Debug)]
+pub enum Operator {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+}
+
+const NUMBERS: [&str; 11] = [
+    "noll", "ett", "två", "tre", "fyra", "fem", "sex", "sju", "åtta", "nio", "tio",
+];
+
+pub fn parse(source_code: &str) -> Result<Vec<Statement>, nom::Err<nom::error::Error<&str>>> {
+    let (_, output) = many1(parse_statement).parse(source_code)?;
+
+    Ok(output)
+}
+
+fn parse_statement(input: &str) -> IResult<&str, Statement> {
+    let (input, output) = alt((parse_print, parse_assignment)).parse(input)?;
+    let (input, _) = cut(tag(".")).parse(input)?;
+    let (input, _) = multispace0(input)?;
+
+    Ok((input, output))
+}
+
+fn parse_print(input: &str) -> IResult<&str, Statement> {
+    let (input, _) = tag("Skriv")(input)?;
+    let (input, _) = space1(input)?;
+    let (input, expression) = parse_expression(input)?;
+
+    Ok((input, Statement::Print { expression }))
+}
+
+fn parse_assignment(input: &str) -> IResult<&str, Statement> {
+    let (input, _) = tag("Spara").parse(input)?;
+    let (input, _) = space1(input)?;
+    let (input, expression) = parse_expression(input)?;
+    let (input, _) = space1(input)?;
+    let (input, _) = tag("i")(input)?;
+    let (input, _) = space1(input)?;
+    let (input, variable) = parse_variable(input)?;
+    Ok((
+        input,
+        Statement::Assignment {
+            variable,
+            expression,
+        },
+    ))
+}
+
+fn parse_expression(input: &str) -> IResult<&str, Expression> {
+    let (input, left) = parse_term.parse(input)?;
+    let (input, op) = opt((space1, parse_operator)).parse(input)?;
+    if let Some((_, op)) = op {
+        let (input, _) = space1(input)?;
+        let (input, right) = parse_expression(input)?;
+
+        Ok((
+            input,
+            Expression::Operation {
+                left: Box::new(Expression::Term(left)),
+                op,
+                right: Box::new(right),
+            },
+        ))
+    } else {
+        Ok((input, Expression::Term(left)))
+    }
+}
+
+fn parse_operator(input: &str) -> IResult<&str, Operator> {
+    let (input, output) =
+        alt((tag("plus"), tag("minus"), tag("gånger"), tag("delat med"))).parse(input)?;
+
+    let operator = match output {
+        "plus" => Operator::Add,
+        "minus" => Operator::Subtract,
+        "gånger" => Operator::Multiply,
+        "delat med" => Operator::Divide,
+        _ => unreachable!(),
+    };
+
+    Ok((input, operator))
+}
+
+fn parse_term(input: &str) -> IResult<&str, Term> {
+    alt((parse_number, parse_string, parse_variable)).parse(input)
+}
+
+fn parse_number(input: &str) -> IResult<&str, Term> {
+    let (input, output) = alt(NUMBERS.map(|n| tag(n))).parse(input)?;
+
+    let number = NUMBERS
+        .iter()
+        .position(|&n| n == output)
+        .map(|n| n as i64)
+        .ok_or(nom::Err::Error(nom::error::Error::new(
+            input,
+            nom::error::ErrorKind::Tag,
+        )))?;
+
+    Ok((input, Term::Number(number)))
+}
+
+fn parse_string(input: &str) -> IResult<&str, Term> {
+    let (input, output) =
+        delimited(tag("\""), take_while1(|c: char| c != '"'), tag("\"")).parse(input)?;
+
+    Ok((input, Term::String(output.to_owned())))
+}
+
+fn parse_variable(input: &str) -> IResult<&str, Term> {
+    let (input, identifier) = take_while1(|c: char| c.is_alphabetic()).parse(input)?;
+
+    Ok((input, Term::Variable(identifier.to_string())))
+}
